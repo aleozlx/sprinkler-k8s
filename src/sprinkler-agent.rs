d87726b5fe281ea8e75e4573348a26864845c247
Alex Yang <aleozlx@gmail.com>
2019-06-25 17:28:03 -0500

    ..

--- src/sprinkler-agent.rs
+++ src/sprinkler-agent.rs
@@ -18,32 +18,37 @@ fn main() {
     
     config::setup_logger(args.occurrences_of("VERBOSE")).expect("Logger Error.");
 
-    const MASTER_ADDR: &str = "bridge.dsa.lan:3777";
-    let mut builder = SprinklerBuilder::new(SprinklerOptions{ master_addr: String::from(MASTER_ADDR), ..Default::default() });
+    tokio::run(futures::future::lazy(|| {
+        const MASTER_ADDR: &str = "bridge.dsa.lan:3777";
+        let mut builder = SprinklerBuilder::new(SprinklerOptions{ master_addr: String::from(MASTER_ADDR), ..Default::default() });
 
-    // parse FNAME_CONFIG and add triggers
-    let sprinklers: Vec<Box<dyn Sprinkler>> = vec![
-        Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-1.dsa.lan"))),
-        Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-2.dsa.lan"))),
-        Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-3.dsa.lan"))),
-        Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-4.dsa.lan"))),
-        Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-5.dsa.lan"))),
-        Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-6.dsa.lan"))),
-        Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-7.dsa.lan"))),
-        Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-8.dsa.lan"))),
-        Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-9.dsa.lan"))),
+        // parse FNAME_CONFIG and add triggers
+        let sprinklers: Vec<Box<dyn Sprinkler>> = vec![
+            Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-1.dsa.lan"))),
+            Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-2.dsa.lan"))),
+            Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-3.dsa.lan"))),
+            Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-4.dsa.lan"))),
+            Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-5.dsa.lan"))),
+            Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-6.dsa.lan"))),
+            Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-7.dsa.lan"))),
+            Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-8.dsa.lan"))),
+            Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-9.dsa.lan"))),
 
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-1.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-2.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-3.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-4.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-5.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-6.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-7.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-8.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-9.dsa.lan")))
-    ];
+            Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-1.dsa.lan"))),
+            Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-2.dsa.lan"))),
+            Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-3.dsa.lan"))),
+            Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-4.dsa.lan"))),
+            Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-5.dsa.lan"))),
+            Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-6.dsa.lan"))),
+            Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-7.dsa.lan"))),
+            Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-8.dsa.lan"))),
+            Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-9.dsa.lan")))
+        ];
 
-    sprinkler_api::agent(&sprinklers);
+        sprinkler_api::agent(&sprinklers);
+        Ok(())
+    }));
+
+    // Prevent the agent from shutting down when there are no async tasks.
     sprinkler_api::loop_forever();
 }
