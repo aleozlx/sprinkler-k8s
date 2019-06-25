f4a049c9a65823007ac9c9a71cf2563923f37ec9
Alex Yang <aleozlx@gmail.com>
2019-06-25 15:10:43 -0500

    ..

--- src/sprinkler-master.rs
+++ src/sprinkler-master.rs
@@ -33,15 +33,15 @@ fn main() {
         Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-8.dsa.lan"))),
         Box::new(builder.build::<CommCheck>(String::from("k-prod-cpu-9.dsa.lan"))),
 
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-1.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-2.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-3.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-4.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-5.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-6.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-7.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-8.dsa.lan"))),
-        Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-9.dsa.lan")))
+        // Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-1.dsa.lan"))),
+        // Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-2.dsa.lan"))),
+        // Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-3.dsa.lan"))),
+        // Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-4.dsa.lan"))),
+        // Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-5.dsa.lan"))),
+        // Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-6.dsa.lan"))),
+        // Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-7.dsa.lan"))),
+        // Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-8.dsa.lan"))),
+        // Box::new(builder.build::<DockerOOM>(String::from("k-prod-cpu-9.dsa.lan")))
     ];
 
     let switch = Switch::new();
