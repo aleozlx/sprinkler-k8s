6e9498d18dc19bbcdcd27f549896ac3c614a07f5
Alex Yang <aleozlx@gmail.com>
2019-07-02 19:51:36 -0500

    ..

--- update-agent.sh
+++ update-agent.sh
@@ -3,5 +3,6 @@ cargo build --release --features sprinkler/master
 for i in $(seq 1 9)
 do
   scp target/release/sprinkler-agent root@k-prod-cpu-${i}.dsa.lan:/usr/local/bin
+  scp systemd/sprinkler-agent.service root@k-prod-cpu-${i}.dsa.lan:/etc/systemd/system/sprinkler-agent.service
 done
 
